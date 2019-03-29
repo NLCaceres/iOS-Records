66fcf6de7d583944289889f4199d67f328b1d324
NLCaceres <NLCaceres21@Gmail.com>
2019-03-29 14:04:12 -0700

    Initial Commit

---
+++ InfectionControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfectionControl
+//
+//  Created by Nick Caceres on 3/29/19.
+//  Copyright © 2019 Nick Caceres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
