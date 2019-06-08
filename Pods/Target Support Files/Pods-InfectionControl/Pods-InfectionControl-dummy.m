b6f16a791c8676ef5afd98515196d6ce6287e23b
NLCaceres <NLCaceres21@Gmail.com>
2019-06-08 00:28:05 -0700

    Recommitting and adding pod files one step at a time

---
+++ Pods/Target Support Files/Pods-InfectionControl/Pods-InfectionControl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InfectionControl : NSObject
+@end
+@implementation PodsDummy_Pods_InfectionControl
+@end
---
+++ Pods/Target Support Files/Pods-InfectionControlUITests/Pods-InfectionControlUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InfectionControlUITests : NSObject
+@end
+@implementation PodsDummy_Pods_InfectionControlUITests
+@end
