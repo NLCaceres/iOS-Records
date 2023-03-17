f3617972b1f9cc1b29186b758f9bf162578ea4d7
NLCaceres <NLCaceres21@Gmail.com>
2023-03-17 11:31:31 -0700

    Add Employee + ReportRepositories into ProfileViewModel

--- InfectionControl/Controllers/ProfileView/ProfileViewModel.swift
+++ InfectionControl/Controllers/ProfileView/ProfileViewModel.swift
@@ -7,6 +7,8 @@
 import UIKit
 
 class ProfileViewModel: ObservableObject {
+    private let employeeRepository: EmployeeRepository
+    private let reportRepository: ReportRepository
     private let networkManager: FetchingNetworkManager
     @Published private(set) var isLoadingEmployee = false
     @Published private(set) var isLoadingTeamList = false
@@ -17,8 +19,11 @@ class ProfileViewModel: ObservableObject {
     @Published var teamList: [Employee] = []
     @Published var reportList: [Report] = [] // Could be individual reports or an entire team's reports
     
-    init(networkManager: FetchingNetworkManager = NetworkManager()) {
+    init(networkManager: FetchingNetworkManager = NetworkManager(), employeeRepository: EmployeeRepository = AppEmployeeRepository(),
+         reportRepository: ReportRepository = AppReportRepository()) {
         self.networkManager = networkManager
+        self.employeeRepository = employeeRepository
+        self.reportRepository = reportRepository
     }
     
     // Adding @MainActor forces SwiftUI Views to update on the MainThread (even though @StateObj + @ObservedObj technically already do)
@@ -27,15 +32,18 @@ class ProfileViewModel: ObservableObject {
         print("Fetching employee info from profileViewModel")
         isLoadingEmployee = true // Publishes change so view can display refreshControl
         // TODO: Save ID in UserDefaults? This ID belongs to logged in user
-        employee = await fetchDTO(endpointPath: "employee/\(employeeID)", for: EmployeeDTO.self, networkManager: networkManager)
-//        employee = Employee(firstName: "Nick", surname: "Caceres", profession: Profession(observedOccupation: "Clinic", serviceDiscipline: "Supervisor"))
+        do {
+            employee = try await employeeRepository.getEmployee(id: employeeID)
+        } catch {
+            print("Got the following error while fetching this employee - \(employeeID): \(error.localizedDescription)")
+        }
         isLoadingEmployee = false
     }
-    @MainActor
+    @MainActor // TODO: To prevent having a NetworkManager dependency, abstract away a image fetching API/dataSource/Repository
     func fetchEmployeeImage(urlPath: String, networkManager: FetchingNetworkManager) async {
         let employeeImgResult = await networkManager.fetchTask(endpointPath: urlPath)
         if case let .failure(error) = employeeImgResult { // Early return if condition
-            print("Got the following error \(error)")
+            print("Got the following error \(error.localizedDescription)")
             return
         }
         let employeeImgData = try! employeeImgResult.get() // Should not be able to throw anymore!
@@ -46,15 +54,23 @@ class ProfileViewModel: ObservableObject {
     func fetchReports(endpointPath: String = "reports") async {
         print("Fetching report info from profileViewModel")
         isLoadingReportList = true
-        let reportOptList = await fetchDTOArr(endpointPath: endpointPath, containing: ReportDTO.self, networkManager: networkManager)
-        reportList = reportOptList.compactMap { $0 }
+        do {
+            reportList = try await reportRepository.getReportList()
+        } catch {
+            print("Got the following error while fetching reports: \(error.localizedDescription)")
+            reportList = []
+        }
         isLoadingReportList = false
     }
     @MainActor
-    func fetchTeam(endpointPath: String = "employees") async {
+    func fetchTeam() async {
         print("Fetching team info from profileViewModel")
         isLoadingTeamList = true
-        teamList = await fetchDTOArr(endpointPath: endpointPath, containing: EmployeeDTO.self, networkManager: networkManager)
+        do {
+            teamList = try await employeeRepository.getEmployeeList()
+        } catch {
+            print("Got the following error while fetching the team of this employee - \(employee?.id ?? "Missing ID"): \(error.localizedDescription)")
+        }
         isLoadingTeamList = false
     }
     
--- InfectionControlTests/ViewModels/ProfileViewModelTests.swift
+++ InfectionControlTests/ViewModels/ProfileViewModelTests.swift
@@ -9,76 +9,87 @@ import XCTest
 import Combine
 
 class ProfileViewModelTests: XCTestCase {
-    var mockSession: MockURLSession!
-    var mockNetworkManager: MockNetworkManager!
-    var viewModel: ProfileViewModel!
+    //TODO: Test fetching employee image
 
-    override func setUpWithError() throws {
-        mockSession = MockURLSession()
-        mockNetworkManager = MockNetworkManager(session: mockSession)
-        viewModel = ProfileViewModel(networkManager: mockNetworkManager)
-    }
-
-    override func tearDownWithError() throws {
-        mockSession = nil
-        mockNetworkManager = nil
-        viewModel = nil
-    }
-
-    func testEmployeeFetch() async throws {
+    func testEmployeeFetch() async throws { // Fetch user's employee profile info
+        let employeeRepository = MockEmployeeRepository(employee: Employee(firstName: "John", surname: "Smith"))
+        let viewModel = ProfileViewModel(
+            networkManager: MockNetworkManager(), employeeRepository: employeeRepository, reportRepository: MockReportRepository()
+        )
         var cancellables = Set<AnyCancellable>()
-        let employee = ModelsFactory.createEmployee(hasProfession: true)
-        mockNetworkManager.replacementData = try! ModelsFactory.encoder().encode(EmployeeDTO(from: employee))
+
         var loadingNumUpdates = 0
-        var modelNumUpdates = 0
-        viewModel.$isLoadingEmployee.sink {
+        viewModel.$isLoadingEmployee.sink { // --F-T-F->
             loadingNumUpdates % 2 == 0 ? XCTAssertFalse($0) : XCTAssertTrue($0)
             loadingNumUpdates += 1
-        }.store(in: &cancellables) // --F-T-F->
-        viewModel.$employee.sink {
-            modelNumUpdates == 0 ? XCTAssertNil($0) : XCTAssertEqual(employee.firstName, $0?.firstName)
+        }.store(in: &cancellables)
+        
+        var modelNumUpdates = 0
+        viewModel.$employee.sink { // --nil--someEmployee-->
+            modelNumUpdates == 0 ? XCTAssertNil($0) : XCTAssertEqual($0?.fullName, "John Smith")
             modelNumUpdates += 1
-        }.store(in: &cancellables) // --nil--someEmployee-->
+        }.store(in: &cancellables)
+        
         await viewModel.fetchEmployeeInfo()
     }
     
     func testTeamFetch() async throws {
+        let employeeRepository = MockEmployeeRepository(
+            employeeList: [Employee(firstName: "John", surname: "Smith"), Employee(firstName: "Melody", surname: "Rios")]
+        )
+        let viewModel = ProfileViewModel(
+            networkManager: MockNetworkManager(), employeeRepository: employeeRepository, reportRepository: MockReportRepository()
+        )
         var cancellables = Set<AnyCancellable>()
-        let employeeDTOs = [EmployeeDTO(from: ModelsFactory.createEmployee())]
-        mockNetworkManager.replacementData = try! ModelsFactory.encoder().encode(employeeDTOs)
+
         var loadingNumUpdates = 0
-        var modelNumUpdates = 0
-        viewModel.$isLoadingTeamList.sink {
+        viewModel.$isLoadingTeamList.sink { // --F-T-F->
             loadingNumUpdates % 2 == 0 ? XCTAssertFalse($0) : XCTAssertTrue($0)
             loadingNumUpdates += 1
-        }.store(in: &cancellables) // --F-T-F->
-        viewModel.$teamList.sink {
+        }.store(in: &cancellables)
+        
+        var modelNumUpdates = 0
+        viewModel.$teamList.sink { // --[]--[employee1, employee2]-->
             if (modelNumUpdates == 0) { XCTAssert($0.isEmpty) }
-            else { XCTAssertEqual(employeeDTOs[0].firstName, $0[0].firstName) }
+            else {
+                XCTAssertEqual($0.count, 2)
+                XCTAssertEqual($0[0].fullName, "John Smith")
+            }
             modelNumUpdates += 1
-        }.store(in: &cancellables) // --nil--someEmployee-->
+        }.store(in: &cancellables)
+        
         await viewModel.fetchEmployeeInfo()
     }
     
     func testReportFetch() async throws {
+        let reportRepository = MockReportRepository(reportList: [
+            Report(employee: Employee(firstName: "John", surname: "Smith"), healthPractice: HealthPractice(name: "Hand Hygiene"),
+                   location: Location(facilityName: "USC", unitNum: "2", roomNum: "123"), date: Date()),
+            Report(employee: Employee(firstName: "Melody", surname: "Rios"), healthPractice: HealthPractice(name: "PPE"),
+                   location: Location(facilityName: "HSC", unitNum: "3", roomNum: "213"), date: Date())
+        ])
+        let viewModel = ProfileViewModel(
+            networkManager: MockNetworkManager(), employeeRepository: MockEmployeeRepository(), reportRepository: reportRepository
+        )
         var cancellables = Set<AnyCancellable>()
-        let reportDTOs = [ReportDTO(from: ModelsFactory.createReport())]
-        mockNetworkManager.replacementData = try! ModelsFactory.encoder().encode(reportDTOs)
+
         var loadingNumUpdates = 0
-        var modelNumUpdates = 0
-        viewModel.$isLoadingReportList.sink {
+        viewModel.$isLoadingReportList.sink { // --F-T-F->
             loadingNumUpdates % 2 == 0 ? XCTAssertFalse($0) : XCTAssertTrue($0)
             loadingNumUpdates += 1
-        }.store(in: &cancellables) // --F-T-F->
-        viewModel.$reportList.sink {
-            if (modelNumUpdates == 0) { XCTAssert($0.isEmpty) }
+        }.store(in: &cancellables)
+        
+        var modelNumUpdates = 0
+        viewModel.$reportList.sink { reportList in // --[]--[report1, report2]-->
+            if (modelNumUpdates == 0) { XCTAssert(reportList.isEmpty) }
             else {
-                XCTAssertEqual(reportDTOs[0].employee?.firstName, $0[0].employee.firstName)
-                XCTAssertEqual(reportDTOs[0].healthPractice?.name, $0[0].healthPractice.name)
-                XCTAssertEqual(reportDTOs[0].location?.facilityName, $0[0].location.facilityName)
+                XCTAssertEqual(reportList.count, 2)
+                XCTAssertEqual(reportList[0].employee.fullName, "John Smith")
+                XCTAssertEqual(reportList[0].healthPractice.name, "Hand Hygiene")
+                XCTAssertEqual(reportList[0].location.facilityName, "USC")
             }
             modelNumUpdates += 1
-        }.store(in: &cancellables) // --nil--someEmployee-->
+        }.store(in: &cancellables)
         await viewModel.fetchEmployeeInfo()
     }
 
