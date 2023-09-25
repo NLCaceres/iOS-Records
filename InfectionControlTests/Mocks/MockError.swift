2e458318c9471ba575f917cd95908c1d4c5fd1a8
NLCaceres <NLCaceres21@Gmail.com>
2023-09-25 12:24:49 -0700

    Add LocalizedError protocol to help MockErrors work more like typical errors

--- InfectionControlTests/Mocks/MockError.swift
+++ InfectionControlTests/Mocks/MockError.swift
@@ -8,7 +8,13 @@ import Foundation
 
 /* Convenient way to throw errors in tests with a helpful description to check for
 * BUT not perfect since in some cases, like URLProtocol, Swift under the hood will cast your error to another type of error
-* like NSURLError, which will drop the description. Consequently, implementing LocalizedError may work even better! */
-enum MockError: Error {
+* like NSURLError, which can drop the description */
+enum MockError: LocalizedError {
     case description(String)
+    var errorDescription: String? {
+        switch self {
+        case let .description(message): // Alternatively access the associated value as "case .description(let message)"
+            return message
+        }
+    }
 }
