af49c2d77ab0b2f673258124ecce8b57fa0dc4f6
NLCaceres <NLCaceres21@Gmail.com>
2023-10-05 17:13:06 -0700

    Rename CollectionView DataSource file + clean up & clarify func usage

--- InfectionControl/Controllers/CreateReportCollectionView/CreateReportCvDataSource.swift
+++ InfectionControl/Controllers/CreateReportCollectionView/CreateReportCvDataSource.swift
@@ -1,37 +1,35 @@
 //
-//  CreateReportCollectionVC+CVDataSource.swift
+//  CreateReportCollectionViewDataSource.swift
 //  InfectionControl
 //
 //  Copyright © 2022 Nick Caceres. All rights reserved.
 
-import Foundation
 import UIKit
 
 // MARK: UICollectionViewDataSource
 extension CreateReportCollectionView {
     
-    // Number of sections (areas with titles followed by buttons)
+    // Number of sections (areas with titles followed by cells/buttons)
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // Two Precaution Types: Quarantine and Normal
+        // Two Precaution Types: Standard and Isolation
         return self.precautions.count
     }
     
-    // Number of buttons per section
-    override func collectionView(_ collectionView: UICollectionView,
-                                 numberOfItemsInSection section: Int) -> Int {
+    // Number of cells/buttons per section
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         guard let healthPractices = self.precautions[section].practices else {
-            return 0 // if arr == 0
+            return 0 // Array is nil so return an no sections needed
         }
-        return healthPractices.count
+        // CDC generally states there are 2 - 8 Standard Precautions and 3-4 Isolation/Transmission precautions
+        return healthPractices.count // So expecting at least 5 cells total divided by 2 sections
     }
     
-    override func collectionView(_ collectionView: UICollectionView,
-                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // Cell setup
         let sectionNum = indexPath.section
         let cellNum = indexPath.item
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MakeNewReportCell",
-                                                      for: indexPath) as! MakeNewReportCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MakeNewReportCell", for: indexPath) as! MakeNewReportCell
+        
         cell.reportTypeImageView.image = #imageLiteral(resourceName: "plus_icon")
         cell.reportTypeImageView.layer.masksToBounds = true
         cell.reportTypeImageView.layer.cornerRadius = cell.bounds.width / 2
@@ -43,22 +41,25 @@ extension CreateReportCollectionView {
         return cell
     }
     
-    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String,
-                                 at indexPath: IndexPath) -> UICollectionReusableView {
+    override func collectionView(_ collectionView: UICollectionView,
+                                 viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind { // This "kind" param lets us search for the Header
         // By using headers in attributes section for collectionView you can find it via the following enum
         case UICollectionView.elementKindSectionHeader:
-            // THEN finally check if the sectionHeader matches our custom type and customize it
+            // THEN finally check if the sectionHeader matches our custom type and initialize it
             guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                    withReuseIdentifier: "ReportTypeSectionHeader", for: indexPath) as? ReportSectionReusableView
-                else { fatalError("Invalid view type") }
+                                                                                   withReuseIdentifier: "ReportTypeSectionHeader",
+                                                                                   for: indexPath) as? ReportSectionReusableView
+            else { fatalError("Invalid view type") }
             
-            if (precautions.isEmpty) { return headerView } // Return early since no unused section needed
+            if (precautions.isEmpty) { return headerView } // Return early since shouldn't render unused/unneeded sections
             
             headerView.backgroundColor = UserDefaults.standard.color(forKey: "themeSecondaryColor")
+            
             let sectionNum = indexPath.section
             headerView.headerLabel.text = "\(precautions[sectionNum].name) Precautions"
             headerView.headerLabel.textColor = UserDefaults.standard.color(forKey: "themeColor")
+            
             return headerView
             
         default: // If made it here, unexpected view was rendered
